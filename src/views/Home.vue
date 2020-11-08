9a2fc80a42fe1d3ebd4dd8fefec93640da596f35
Guilherme <guilherme@astrusweb.com>
2020-11-08 11:20:16 -0300

    add tagmanager

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -139,6 +139,20 @@ export default {
     fetch(apiURL).then((response) => response.json()).then((result) => {
       this.messages = result.results;
     });
+    const gtmScript = document.createElement('script');
+    gtmScript.type = 'text/javascript';
+    const gtmCode = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K57247W');`;
+    try {
+      gtmScript.appendChild(document.createTextNode(gtmCode));
+      document.head.appendChild(gtmScript);
+    } catch (e) {
+      gtmScript.text = gtmCode;
+      document.head.appendChild(gtmScript);
+    }
   },
   methods: {
     addMessage() {
