01268cbb6f665874c8a1a6a58c2ef14123433a6d
Guilherme <guilherme@astrusweb.com>
2021-03-02 19:40:52 -0300

    fix uso do placeholder qnd imageurl tem link inválido

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -159,12 +159,12 @@
 import ReplyBox from '../components/replybox.vue';
 import Message from '../components/message.vue';
 
-// const apiURL = 'https://gchan-message-board.herokuapp.com/messages';
-const apiURL = 'https://gchan-message-board.herokuapp.com/messages/';
-const repliesURL = 'https://gchan-message-board.herokuapp.com/replies';
-const imgurURLimg = 'https://gchan-message-board.herokuapp.com/imgupload';
-const imgurURLgif = 'https://gchan-message-board.herokuapp.com/gifupload';
-const imgurURLupload = 'https://gchan-message-board.herokuapp.com/videoupload';
+// const apiURL = 'http://localhost:5000/messages';
+const apiURL = 'http://localhost:5000/messages/';
+const repliesURL = 'http://localhost:5000/replies';
+const imgurURLimg = 'http://localhost:5000/imgupload';
+const imgurURLgif = 'http://localhost:5000/gifupload';
+const imgurURLupload = 'http://localhost:5000/videoupload';
 export default {
   name: 'Home',
   components: {
@@ -696,7 +696,7 @@ export default {
     },
     removeUpload(e) {
       const deleteHash = e.target.getAttribute('data-deletehash').trim();
-      fetch(`https://gchan-message-board.herokuapp.com/imgur/${deleteHash}`, {
+      fetch(`http://localhost:5000/imgur/${deleteHash}`, {
         method: 'DELETE',
         headers: {
           Authorization: 'Client-ID 3435e574a9859d1',
