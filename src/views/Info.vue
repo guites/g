a1bbef9238bb38b4346a9d102c79cba9d72f7c25
Guilherme <guilherme@astrusweb.com>
2021-03-02 23:58:53 -0300

    adc clique para citar respostas dentro de um post

--- src/views/Info.vue
+++ src/views/Info.vue
@@ -58,7 +58,7 @@ export default {
     },
   },
   data: () => ({
-    SERVERurl: 'http://localhost:5000',
+    SERVERurl: 'https://gchan-message-board.herokuapp.com',
     show_pwd_checked: '',
     name: '',
     email: '',
