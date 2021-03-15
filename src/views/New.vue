699497f9895a98f43a9c3e0d1a1e070cf012afcf
Guilherme <guilherme@astrusweb.com>
2021-03-15 18:56:38 -0300

    fix logo on header

--- src/views/New.vue
+++ src/views/New.vue
@@ -72,7 +72,7 @@
         >
         </video>
         <button @click="toggleAudioOnClick" type="button" class="volume">
-          <img src="http://localhost:8080/volume-off.png" alt="Volume">
+          <img src="https://gchan.com.br/volume-off.png" alt="Volume">
         </button>
       </div>
       <p v-else>Formato não suportado! ::(</p>
@@ -82,7 +82,7 @@
 </template>
 
 <script>
-const apiURL = 'http://localhost:5000/messages/';
+const apiURL = 'https://gchan-message-board.herokuapp.com/messages/';
 export default {
   name: 'Home',
   props: {
@@ -172,9 +172,9 @@ export default {
       console.log(video);
       video.muted = !video.muted;
       if (video.muted) {
-        audioImg.src = 'http://localhost:8080/volume-off.png';
+        audioImg.src = 'https://gchan.com.br/volume-off.png';
       } else {
-        audioImg.src = 'http://localhost:8080/volume-high.png';
+        audioImg.src = 'https://gchan.com.br/volume-high.png';
       }
     },
     toggleSubject() {
