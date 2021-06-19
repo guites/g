4dc74af238e7fa8f20e309783d1619803e8ec831
Guilherme <guilherme@astrusweb.com>
2021-06-19 18:31:03 -0300

    teste beforeEach nas routes e catch all

--- src/router/index.js
+++ src/router/index.js
@@ -42,6 +42,11 @@ const routes = [
     name: 'New post',
     component: New,
   },
+  {
+    path: '/:catchAll(.*)',
+    component: Home,
+    name: 'Home'
+  }
 ];
 
 const router = new VueRouter({
@@ -49,14 +54,14 @@ const router = new VueRouter({
   routes,
 });
 
-// router.beforeEach((to, from, next) => {
-//   // Redirect if fullPath begins with a hash (ignore hashes later in path)
-//   if (to.fullPath.substr(0, 2) === '/#') {
-//     const path = to.fullPath.substr(2);
-//     next(path);
-//     return;
-//   }
-//   next();
-// });
+router.beforeEach((to, from, next) => {
+  // Redirect if fullPath begins with a hash (ignore hashes later in path)
+  if (to.fullPath.substr(0, 2) === '/#') {
+    const path = to.fullPath.substr(2);
+    next(path);
+    return;
+  }
+  next();
+});
 
 export default router;
