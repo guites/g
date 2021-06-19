4a4e9931d462015ad9c5f0f41530f53a9f229cac
Guilherme <guilherme@astrusweb.com>
2021-06-19 17:30:14 -0300

    teste usando routes estilo history

--- docs/js/about.ea3b9ebc.js
+++ docs/js/about.ea3b9ebc.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(e,t,n){"use strict";n.r(t);var s=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("Send Messages. Debug posts.")])])}],u=n("2877"),c={},l=Object(u["a"])(c,s,a,!1,null,null,null);t["default"]=l.exports}}]);
-//# sourceMappingURL=about.f86bebd2.js.map
+//# sourceMappingURL=about.ea3b9ebc.js.map
--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 import Vue from 'vue';
+import Vuex from 'vuex';
 import App from './App.vue';
 import router from './router';
 
+Vue.use(Vuex);
 Vue.config.productionTip = false;
+
+const store = new Vuex.Store();
 
 new Vue({
   router,
+  store,
   render(h) { return h(App); },
+  created () {
+    if (sessionStorage.redirect) {
+      const redirect = sessionStorage.redirect;
+      delete sessionStorage.redirect;
+      this.$router.push(redirect);
+    }
+  }
 }).$mount('#app');
