f4f4ee282c6f5f1438ed3ba3f4a743fac6e5f5dc
guites <gui.garcia67@gmail.com>
2022-11-11 13:19:48 -0300

    remove unused About and New pages

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,6 @@ import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
 import Info from '../views/Info.vue';
 import Post from '../views/Post.vue';
-import New from '../views/New.vue';
-import About from '../views/About.vue';
 
 Vue.use(VueRouter);
 
@@ -13,11 +11,6 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: About,
   },
   {
     path: '/info',
@@ -32,11 +25,6 @@ const routes = [
       const id = Number.parseInt(route.params.id, 10);
       return { id };
     },
-  },
-  {
-    path: '/new',
-    name: 'New post',
-    component: New,
   },
 ];
 
