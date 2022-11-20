bc9b17a0d2e93c9620c203dd0fb7efb9fbabc400
Guilherme Garcia <gui.garcia67@gmail.com>
2022-11-20 13:10:53 -0300

    fix rebase error

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -378,11 +378,7 @@ export default {
           .then((result) => {
             if (result.results) {
               this.message = this.sanitizeSingleMessage(result.results.shift());
-<<<<<<< HEAD
-              fetch(`${this.$backendURL}${this.messageURL}/${this.id}/replies`).then((response) => response.json())
-=======
               fetch(`${this.$backendURL}${this.messageURL}/${this.id}/${this.repliesURL}`).then((response) => response.json())
->>>>>>> 0ae6622 (fix message and reply get url in post single page)
                 .then((replies) => {
                   if (replies.error) {
                     return;
