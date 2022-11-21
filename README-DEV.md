e06757ed39af60c4b8fd1635e9bcb7db93b31131
Guilherme Garcia <gui.garcia67@gmail.com>
2022-11-20 21:34:36 -0300

    Add instructions for deployment

--- README-DEV.md
+++ README-DEV.md
@@ -47,3 +47,20 @@ Please inspect the script for further information, and run it using
 This project uses sass and compiles it with [gulp](https://gulpjs.com).
 
 You can edit the files in src/assets/sass and generate the css files with `npm run sass`.
+
+## Building for deployment
+
+The files can be built for deployment by running
+
+                npm run build
+
+Make sure to switch the `projectURL` and `backendURL` global variables found in `src/main.js` to the production URLs.
+
+                Vue.prototype.$projectURL = 'https://gchan.com.br/';
+                Vue.prototype.$backendURL = 'https://gchan-backend.com.br';
+
+The `docker-start.sh` script has a helper function, `./docker-start -b` which automatically switches the URLs and builds the files into the `./dist` directory.
+
+After building, you can deploy the files to any http server, for example, with node's [http-server](https://www.npmjs.com/package/http-server) package:
+
+        cd /dist && npx http-server .
--- docker-start.sh
+++ docker-start.sh
@@ -12,6 +12,7 @@ Help()
    echo "u     Runs docker-compose up on the project services."
    echo "d     Runs docker-compose down on the project services."
    echo "s     Starts the front-end development server."
+   echo "b     Builds the app for deployment into the /dist directory."
    echo
 }
 
@@ -23,20 +24,36 @@ down_container() {
   docker compose -f .devcontainer/docker-compose.yml down
 }
 
+npm_install() {
+  docker exec -w /workspace gchan-frontend-app-1 npm install
+}
+
 run_dev_server() {
   docker exec -it -w /workspace gchan-frontend-app-1 bash -c "npm run dev || exit 1"
+}
+
+run_build() {
+  docker exec -w /workspace gchan-frontend-app-1 npm run build
+}
+
+build_deployment_files() {
+  .devcontainer/post-start.sh prod
+  if ! run_build; then
+    npm_install
+    run_build
+  fi
 }
 
 start_dev_server() {
   .devcontainer/post-start.sh dev
   if ! run_dev_server; then
-    docker exec -w /workspace gchan-frontend-app-1 npm install
+    npm_install
     run_dev_server
   fi
 }
 
 # handle script options
-while getopts ":uds" option; do
+while getopts ":udsb" option; do
    case $option in
       u) # ups the container
         up_container
@@ -46,6 +63,9 @@ while getopts ":uds" option; do
         exit;;
       s) # starts the development server
         start_dev_server
+        exit;;
+      b) # builds necessary files for deployment
+        build_deployment_files
         exit;;
       \?) # invalid option
         echo "Invalid option!"
