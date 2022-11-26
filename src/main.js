03c870794157c3f7ef86c62c1615124cd9f0e7d8
Guilherme Garcia <gui.garcia67@gmail.com>
2022-11-26 10:55:18 -0300

    Add vuetify dependencies

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false;
 Vue.prototype.$projectURL = 'https://gchan.com.br/';
@@ -9,5 +10,6 @@ Vue.prototype.$captchaClient = '6LfB04AaAAAAAGTm-ljshaykXuT9YiePLxgqy471';
 
 new Vue({
   router,
+  vuetify,
   render(h) { return h(App); }
 }).$mount('#app');
