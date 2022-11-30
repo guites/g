141af412d4f28031c9132c5e6cf0aa6ad060ae90
Guilherme Garcia <gui.garcia67@gmail.com>
2022-11-30 11:01:30 -0300

    make the logo a link to the home page

--- src/components/appbar.vue
+++ src/components/appbar.vue
@@ -2,16 +2,18 @@
   <v-app-bar app>
     <div class="d-flex align-center">
       <v-toolbar-title><h1>gchan</h1></v-toolbar-title>
-      <v-img
-        round
-        alt="Vuetify Logo"
-        class="shrink mr-2 rounded"
-        contain
-        src="@/assets/sham-48.png"
-        transition="scale-transition"
-        width="32"
-        style="border: 2px solid #a4a4a4"
-      />
+      <router-link class="d-flex align-center" to="/">
+        <v-img
+          round
+          alt="Vuetify Logo"
+          class="shrink mr-2 rounded"
+          contain
+          src="@/assets/sham-48.png"
+          transition="scale-transition"
+          width="32"
+          style="border: 2px solid #a4a4a4"
+        />
+      </router-link>
     </div>
     <v-spacer></v-spacer>
     <v-btn href="https://github.com/guites/g" target="_blank" text>
