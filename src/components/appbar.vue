90666f89929815d8a90da69810633caf817e2793
Guilherme Garcia <gui.garcia67@gmail.com>
2022-12-01 23:21:02 -0300

    Add link to home page on logo

--- src/components/appbar.vue
+++ src/components/appbar.vue
@@ -1,20 +1,32 @@
 <template>
   <v-app-bar app>
-    <div class="d-flex align-center">
-      <v-toolbar-title><h1>gchan</h1></v-toolbar-title>
-      <router-link class="d-flex align-center" to="/">
-        <v-img
-          round
-          alt="Vuetify Logo"
-          class="shrink mr-2 rounded"
-          contain
-          src="@/assets/sham-48.png"
-          transition="scale-transition"
-          width="32"
-          style="border: 2px solid #a4a4a4"
-        />
-      </router-link>
-    </div>
+    <v-toolbar-title>
+      <h1>gchan</h1>
+    </v-toolbar-title>
+    <v-tooltip v-model="imgTooltip" right>
+      <template v-slot:activator="{ on, attrs }">
+        <router-link
+          @focus.native="imgTooltip = true"
+          @blur.native="imgTooltip = false"
+          class="text-decoration-none d-flex align-center ma-4"
+          to="/"
+        >
+          <v-img
+            round
+            v-bind="attrs"
+            v-on="on"
+            alt="gchan logo"
+            class="shrink rounded"
+            contain
+            src="@/assets/sham-48.png"
+            width="32"
+            style="border: 2px solid #a4a4a4"
+          />
+        </router-link>
+      </template>
+      <span v-if="$route.name === 'Home'">Bem vindo ao gchan!</span>
+      <span v-else>Voltar pra home</span>
+    </v-tooltip>
     <v-spacer></v-spacer>
     <v-btn href="https://github.com/guites/g" target="_blank" text>
       <span class="mr-2">Source code</span>
@@ -25,5 +37,8 @@
 <script>
 export default {
   name: "AppBar",
+  data: () => ({
+    imgTooltip: false,
+  }),
 };
 </script>
